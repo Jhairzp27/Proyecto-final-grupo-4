8b820d503a56a935f610ff6606fa10b20171c7f6
SebastianSarasti <sebastiansarasti.14@gmail.com>
2023-12-30 11:29:15 -0500

    menu

--- ProyectoFinalGrupo4/src/App.java
+++ ProyectoFinalGrupo4/src/App.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Menu");
+        System.out.println("1. Transferir dinero \n 2. Pagar servicios\n + 3. Consultar Saldos  ");
+        System.out.println("4. Invertir \n 5. Recargar celular\n + 6. Cambiar contraseña  ");
+        System.out.println("7. Estado de Cuenta \n 8. Imprimir Estado de Cuenta ");
+        int opcionMenu = Integer.parseInt(sc.nextLine());
+
+        switch (opcionMenu) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+
+                break;
+            case 8:
+
+                break;
+
+            default:
+                break;
+        }
     }
+
 }
