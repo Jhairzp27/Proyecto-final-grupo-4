eeb3fba8bfdf3b1e9827a87227fa1dc83cc6e136
Jhordyp36 <jhordyp2004@gmail.com>
2024-02-29 22:31:22 -0500

    Comentación UsuarioBL

--- src/BusinessLogic/UsuarioBL.java
+++ src/BusinessLogic/UsuarioBL.java
@@ -7,8 +7,9 @@ import DataAccess.UsuarioDAO;
 import DataAccess.DTO.UsuarioDTO;
 
 /**
- * La clase `UsuarioBL` actúa como una capa de lógica de negocios para operaciones relacionadas con el
- * usuario al interactuar con un objeto `UsuarioDAO`.
+ * La clase `UsuarioBL` en Java contiene métodos para el registro de usuarios, lectura de datos de
+ * usuarios, actualización de información de usuarios, eliminación de usuarios e inicio de sesión de
+ * usuarios utilizando un Objeto de transferencia de datos (DTO) y un Objeto de acceso a datos (DAO).
  */
 public class UsuarioBL {
     private UsuarioDTO usuarioDTO;
@@ -17,23 +18,22 @@ public class UsuarioBL {
     public UsuarioBL() {}
     
     /**
-     * La función "crear" en Java toma un objeto UserDTO como parámetro y llama al método de creación
-     * en userDAO para crear un nuevo usuario.
+     * La función `registrador` en Java intenta crear un nuevo usuario utilizando el objeto
+     * `UsuarioDTO` proporcionado y devuelve un valor booleano que indica el éxito de la operación.
      * 
-     * @param usuarioDTO El parámetro `usuarioDTO` es un objeto de tipo `UsuarioDTO` que contiene los
-     * datos necesarios para crear un nuevo usuario en el sistema. Es probable que este objeto incluya
-     * información como el nombre del usuario, correo electrónico, contraseña y cualquier otro detalle
-     * relevante necesario para la creación del usuario.
-     * @return El método devuelve un valor booleano, que es el resultado de llamar al método `crear` en
-     * el objeto `usuarioDAO` con el parámetro `usuarioDTO`.
+     * @param usuarioDTO El parámetro `usuarioDTO` es un objeto de tipo `UsuarioDTO`, que probablemente
+     * contiene información sobre un usuario como su nombre, correo electrónico, contraseña, etc. Este
+     * objeto se utiliza como entrada al método `registrar` para crear un nuevo usuario en el sistema
+     * pasándolo al `
+     * @return El método "registrador" devuelve un valor booleano.
      */
-    public boolean crear(UsuarioDTO usuarioDTO) throws Exception {
+    public boolean registrar(UsuarioDTO usuarioDTO) throws Exception {
         return usuarioDAO.crear(usuarioDTO);
     }
 
     /**
-     * Esta función Java lee todos los datos del usuario de la base de datos utilizando UsuarioDAO y
-     * los devuelve como una lista de objetos UsuarioDTO.
+     * Esta función Java lee todos los datos del usuario de la base de datos utilizando la clase
+     * UsuarioDAO y los devuelve como una lista de objetos UsuarioDTO.
      * 
      * @return Se devuelve una lista de objetos UsuarioDTO.
      */
@@ -41,14 +41,15 @@ public class UsuarioBL {
         return usuarioDAO.leerTodo();
     }
 
-    /**
-     * Esta función de Java lee un usuario por su ID utilizando un objeto de transferencia de datos
-     * (DTO) y un objeto de acceso a datos (DAO).
-     * 
-     * @param idUsuario El parámetro `idUsuario` es un valor Entero que representa el identificador
-     * único de un usuario en el sistema.
-     * @return El método devuelve un objeto de tipo UsuarioDTO.
-     */
+   /**
+    * Esta función de Java lee un usuario por su ID utilizando un objeto de transferencia de datos
+    * (DTO) y una clase DAO.
+    * 
+    * @param idUsuario El parámetro `idUsuario` es un valor Entero que representa el identificador
+    * único de un usuario en el sistema. El método `leerPorId` está diseñado para recuperar un objeto
+    * `UsuarioDTO` de la base de datos en función de este ID de usuario.
+    * @return El método devuelve un objeto UsuarioDTO.
+    */
     public UsuarioDTO leerPorId(Integer idUsuario) throws Exception {
         usuarioDTO = usuarioDAO.leerPor(idUsuario);
         return usuarioDTO;
@@ -59,8 +60,7 @@ public class UsuarioBL {
      * devuelve un objeto UserDTO.
      * 
      * @param username El método `leerPorUsername` toma un parámetro `String` llamado `username`, que
-     * se utiliza para buscar un usuario en la base de datos según su nombre de usuario. Luego, el
-     * método recupera la información del usuario en forma de objeto `UsuarioDTO` y la devuelve.
+     * se utiliza para buscar un usuario en la base de datos según su nombre de usuario.
      * @return El método devuelve un objeto UsuarioDTO.
      */
     public UsuarioDTO leerPorUsername(String username) throws Exception {
@@ -69,12 +69,12 @@ public class UsuarioBL {
     }
 
     /**
-     * Esta función Java lee una lista de objetos UsuarioDTO excluyendo al usuario actual según la ID
-     * de usuario proporcionada.
+     * Esta función Java lee una lista de objetos UsuarioDTO excluyendo el correspondiente al
+     * idUsuarioLogeado proporcionado.
      * 
-     * @param idUsuarioLogeado El parámetro `idUsuarioLogeado` representa el ID del usuario actualmente
-     * conectado. Este método `leerSinUsuarioActual` se utiliza para recuperar una lista de objetos
-     * `UsuarioDTO` excluyendo al usuario con el ID especificado.
+     * @param idUsuarioLogeado El parámetro `idUsuarioLogeado` representa el ID del usuario que
+     * actualmente está conectado. Este método `leerSinUsuarioActual` está diseñado para leer y
+     * devolver una lista de objetos `UsuarioDTO` excluyendo al usuario que está actualmente conectado.
      * @return Se devuelve una ArrayList de objetos UsuarioDTO.
      */
     public ArrayList<UsuarioDTO> leerSinUsuarioActual(Integer idUsuarioLogeado) throws Exception {
@@ -82,11 +82,11 @@ public class UsuarioBL {
     }
 
     /**
-     * La función `actualizar` en Java actualiza la información de un usuario utilizando un Objeto de
-     * Transferencia de Datos (DTO) y devuelve un booleano que indica el éxito de la operación.
+     * Esta función de Java actualiza la información de un usuario utilizando un objeto de
+     * transferencia de datos (DTO) y devuelve un valor booleano que indica el éxito de la operación.
      * 
      * @param usuarioDTO El parámetro `usuarioDTO` es un objeto de tipo `UsuarioDTO` que contiene los
-     * datos de un usuario que necesita ser actualizado en la base de datos.
+     * datos necesarios para actualizar un usuario en la base de datos.
      * @return El método devuelve un valor booleano, que es el resultado de llamar al método
      * `actualizar` sobre el objeto `usuarioDAO` con el parámetro `usuarioDTO`.
      */
@@ -98,8 +98,7 @@ public class UsuarioBL {
      * Esta función de Java elimina un usuario con la ID especificada utilizando un método DAO.
      * 
      * @param idUsuario El parámetro `idUsuario` es un valor Entero que representa el identificador
-     * único de un usuario que desea eliminar del sistema. Este método intenta eliminar al usuario con
-     * el `idUsuario` especificado llamando al método `eliminar` del objeto `usuarioDAO`.
+     * único de un usuario que desea eliminar del sistema.
      * @return Se devuelve un valor booleano.
      */
     public boolean eliminar(Integer idUsuario) throws Exception {
@@ -107,16 +106,15 @@ public class UsuarioBL {
     }
 
     /**
-     * La función `logear` intenta iniciar sesión en un usuario con el nombre de usuario y contraseña
-     * proporcionados llamando al método `loginUsuario` del objeto `usuarioDAO`.
+     * Esta función de Java intenta iniciar sesión como usuario llamando a un método en la clase
+     * usuarioDAO con el nombre de usuario y la contraseña proporcionados.
      * 
      * @param username El parámetro "nombre de usuario" es una cadena que representa el nombre de
      * usuario del usuario que intenta iniciar sesión.
      * @param password El parámetro `contraseña` en el método `logear` es de tipo String. Se utiliza
      * para almacenar la entrada de contraseña proporcionada por el usuario con fines de autenticación.
-     * @return El método `logear` devuelve un valor booleano, que es el resultado de llamar al método
-     * `loginUsuario` desde el objeto `usuarioDAO` con los parámetros `nombre de usuario` y
-     * `contraseña` proporcionados.
+     * @return El método devuelve un valor booleano, que indica si el intento de inicio de sesión fue
+     * exitoso o no.
      */
     public boolean logear(String username, String password) throws Exception {
         return usuarioDAO.loginUsuario(username, password);
