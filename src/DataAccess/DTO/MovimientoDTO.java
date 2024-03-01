4b03e372739e623fa5672e92392761f4cdd452f1
Angel Pastaz <angelpstaz@gmail.com>
2024-02-29 22:37:19 -0500

    Documentacion DTO

--- src/DataAccess/DTO/MovimientoDTO.java
+++ src/DataAccess/DTO/MovimientoDTO.java
@@ -1,5 +1,9 @@
 package DataAccess.DTO;
 
+/**
+ * La clase `MovimientoDTO` representa un objeto de transferencia de datos para una transacción
+ * financiera con atributos como ID de transferencia, usuario emisor, usuario receptor, monto y fecha.
+ */
 public class MovimientoDTO {
     private Integer IdTransferencia;
     private String  UsuarioEnvia;
@@ -7,6 +11,8 @@ public class MovimientoDTO {
     private Float   Monto;
     private String  Fecha;
 
+    public MovimientoDTO() {}
+    
     public MovimientoDTO(Integer idTransferencia,
                          String usuarioEnvia, 
                          String usuarioRecibe, 
@@ -19,49 +25,100 @@ public class MovimientoDTO {
         Fecha           = fecha;
     }
 
+    /**
+     * Esta función Java devuelve el IdTransferencia como un Número Entero.
+     * 
+     * @return El método `getIdTransferencia` devuelve un valor entero que representa la
+     * IdTransferencia.
+     */
     public Integer getIdTransferencia() {
         return IdTransferencia;
     }
 
+    /**
+     * Esta función Java establece el valor de la variable IdTransferencia.
+     * 
+     * @param idTransferencia El parámetro `idTransferencia` es un valor Entero que representa el ID de
+     * una transferencia.
+     */
     public void setIdTransferencia(Integer idTransferencia) {
         IdTransferencia = idTransferencia;
     }
 
+    /**
+     * La función `getUserSubmission` en Java devuelve el valor de la variable `UserSubmission`.
+     * 
+     * @return El método `getUsuarioEnvia` está devolviendo el valor de la variable `UsuarioEnvia`.
+     */
     public String getUsuarioEnvia() {
         return UsuarioEnvia;
     }
 
+    /**
+     * La función establece el valor de la variable "UsuarioEnvia" al parámetro "usuarioEnvia"
+     * proporcionado.
+     * 
+     * @param usuarioEnvia El parámetro `usuarioEnvia` es un String que representa al usuario que está
+     * enviando un mensaje o realizando una acción. En el fragmento de código proporcionado, el método
+     * `setUsuarioEnvia` se utiliza para establecer el valor de la variable `UsuarioEnvia` en la cadena
+     * `usuarioEnvia` especificada.
+     */
     public void setUsuarioEnvia(String usuarioEnvia) {
         UsuarioEnvia = usuarioEnvia;
     }
 
+    /**
+     * La función `getUsuarioRecibe` en Java devuelve el valor de la variable `UsuarioRecibe`.
+     * 
+     * @return El método `getUsuarioRecibe` está devolviendo el valor de la variable `UsuarioRecibe`.
+     */
     public String getUsuarioRecibe() {
         return UsuarioRecibe;
     }
 
+    /**
+     * La función establece el valor de la variable "UsuarioRecibe" en Java.
+     * 
+     * @param usuarioRecibe El parámetro "usuarioRecibe" es un String que representa el nombre de
+     * usuario del usuario que recibirá algo.
+     */
     public void setUsuarioRecibe(String usuarioRecibe) {
         UsuarioRecibe = usuarioRecibe;
     }
 
+    // El método `getMonto()` de la clase `MovimientoDTO` es un método getter en Java que se utiliza
+    // para recuperar el valor del atributo `Monto`, que representa el monto de una transacción
+    // financiera.
     public Float getMonto() {
         return Monto;
     }
 
+    // El método `setMonto(Float monto)` en la clase `MovimientoDTO` es un método setter en Java que se
+    // utiliza para establecer el valor del atributo `Monto`, que representa el monto de una
+    // transacción financiera.
     public void setMonto(Float monto) {
         Monto = monto;
     }
 
+    // El método `getFecha()` de la clase `MovimientoDTO` es un método getter en Java que se utiliza
+    // para recuperar el valor del atributo `Fecha`, que representa la fecha de una transacción
+    // financiera. Este método devuelve el valor del atributo `Fecha` cuando se llama.
     public String getFecha() {
         return Fecha;
     }
 
+    // El método `setFecha(String fecha)` en la clase `MovimientoDTO` es un método setter en Java que
+    // se utiliza para establecer el valor del atributo `Fecha`, que representa la fecha de una
+    // transacción financiera.
     public void setFecha(String fecha) {
         Fecha = fecha;
     }
 
-    public MovimientoDTO() {}
 
     @Override
+    // El método `public String toString()` en Java es un método anulado de la clase `Object` que se
+    // utiliza para devolver una representación de cadena de un objeto. Cuando llamas a `toString()` en
+    // un objeto, devuelve una cadena que normalmente incluye información sobre el estado del objeto.
     public String toString() {
         return "\n" + getClass().getName() 
              + "\n IdTransferencia: " + getIdTransferencia()
--- src/DataAccess/DTO/TransferenciaDTO.java
+++ src/DataAccess/DTO/TransferenciaDTO.java
@@ -1,5 +1,10 @@
 package DataAccess.DTO;
 
+/**
+ * La clase `TransferenciaDTO` representa un objeto de transferencia de datos para una transacción
+ * financiera con varios atributos como ID de transferencia, ID del remitente, ID del receptor, monto,
+ * fecha y estado.
+ */
 public class TransferenciaDTO {
     private Integer IdTransferencia;
     private Integer IdUsuarioEnvia;
@@ -42,70 +47,150 @@ public class TransferenciaDTO {
         Fecha           = fecha;
     }
 
+    /**
+     * Esta función Java devuelve el IdTransferencia como un Número Entero.
+     * 
+     * @return El método `getIdTransferencia` devuelve un valor `Entero` que representa la
+     * `IdTransferencia`.
+     */
     public Integer getIdTransferencia() {
         return IdTransferencia;
     }
     
+    /**
+     * La función setIdTransferencia establece el valor de la variable IdTransferencia.
+     * 
+     * @param idTransferencia El parámetro `idTransferencia` es un valor Entero que representa el ID de
+     * una transferencia.
+     */
     public void setIdTransferencia(Integer idTransferencia) {
         IdTransferencia = idTransferencia;
     }
     
+    /**
+     * La función devuelve el IdUsuarioEnvia como un Entero.
+     * 
+     * @return El método devuelve un valor entero que representa la identificación del usuario que
+     * envió el mensaje.
+     */
     public Integer getIdUsuarioEnvia() {
         return IdUsuarioEnvia;
     }
     
+    /**
+     * La función setIdUsuarioEnvia establece el valor de la variable IdUsuarioEnvia en Java.
+     * 
+     * @param idUsuarioEnvia El parámetro `idUsuarioEnvia` es un Entero que representa el ID del
+     * usuario que envía un mensaje o realiza una acción.
+     */
     public void setIdUsuarioEnvia(Integer idUsuarioEnvia) {
         IdUsuarioEnvia = idUsuarioEnvia;
     }
     
+    /**
+     * La función devuelve el Id del usuario que recibe un mensaje.
+     * 
+     * @return El método devuelve un valor entero que representa la identificación del usuario que
+     * recibe algo.
+     */
     public Integer getIdUsuarioRecibe() {
         return IdUsuarioRecibe;
     }
     
+    /**
+     * Esta función Java establece el valor de la variable "IdUsuarioRecibe" al valor entero
+     * proporcionado.
+     * 
+     * @param idUsuarioRecibe El parámetro `idUsuarioRecibe` es un valor Entero que representa el ID
+     * del usuario que está recibiendo algo.
+     */
     public void setIdUsuarioRecibe(Integer idUsuarioRecibe) {
         IdUsuarioRecibe = idUsuarioRecibe;
     }
     
+    // El método `getMonto()` en la clase `TransferenciaDTO` es un método getter para recuperar el
+    // valor del atributo `Monto`, que representa el monto de la transacción financiera. Este método
+    // devuelve un valor `Float` correspondiente al monto de la transacción almacenado en el atributo
+    // `Monto` del objeto `TransferenciaDTO`.
     public Float getMonto() {
         return Monto;
     }
     
+    // El método `public void setMonto(Float monto)` en la clase `TransferenciaDTO` es un método de
+    // establecimiento utilizado para establecer el valor del atributo `Monto`, que representa el monto
+    // de una transacción financiera.
     public void setMonto(Float monto) {
         Monto = monto;
     }
     
+    // El método `getFecha()` en la clase `TransferenciaDTO` es un método getter que se utiliza para
+    // recuperar el valor del atributo `Fecha`, que representa la fecha de una transacción financiera.
+    // Este método devuelve un valor `String` correspondiente a la fecha almacenada en el atributo
+    // `Fecha` del objeto `TransferenciaDTO`.
     public String getFecha() {
         return Fecha;
     }
     
+    // El método `public void setFecha(String fecha)` en la clase `TransferenciaDTO` es un método
+    // setter utilizado para establecer el valor del atributo `Fecha`, que representa la fecha de una
+    // transacción financiera.
     public void setFecha(String fecha) {
         Fecha = fecha;
     }
     
+    // El método `public String getEstado() {` en la clase `TransferenciaDTO` es un método getter que
+    // se utiliza para recuperar el valor del atributo `Estado`, que representa el estado de una
+    // transacción financiera. Este método devuelve un valor `String` correspondiente al estado
+    // almacenado en el atributo `Estado` del objeto `TransferenciaDTO`.
     public String getEstado() {
         return Estado;
     }
     
+    // El método `public void setEstado(String estado)` en la clase `TransferenciaDTO` es un método de
+    // establecimiento utilizado para establecer el valor del atributo `Estado`, que representa el
+    // estado de una transacción financiera. Este método toma un parámetro `String` `estado` y lo
+    // asigna al atributo `Estado` del objeto `TransferenciaDTO`, actualizando efectivamente el estado
+    // de la transacción.
     public void setEstado(String estado) {
         Estado = estado;
     }
     
+    // El método `public String getFechaCrea() {` en la clase `TransferenciaDTO` es un método getter
+    // utilizado para recuperar el valor del atributo `FechaCrea`, que representa la fecha de creación
+    // de una transacción financiera. Este método devuelve un valor `String` correspondiente a la fecha
+    // de creación almacenada en el atributo `FechaCrea` del objeto `TransferenciaDTO`.
     public String getFechaCrea() {
         return FechaCrea;
     }
     
+    // El método `public void setFechaCrea(String fechaCrea)` en la clase `TransferenciaDTO` es un
+    // método setter utilizado para establecer el valor del atributo `FechaCrea`, que representa la
+    // fecha de creación de una transacción financiera. Este método toma un parámetro `String
+    // fechaCrea` y lo asigna al atributo `FechaCrea` del objeto `TransferenciaDTO`, actualizando
+    // efectivamente la fecha de creación de la transacción.
     public void setFechaCrea(String fechaCrea) {
         FechaCrea = fechaCrea;
     }
     
+    // El método `public String getFechaModifica() {` en la clase `TransferenciaDTO` es un método
+    // getter utilizado para recuperar el valor del atributo `FechaModifica`, que representa la fecha
+    // de modificación de una transacción financiera. Este método devuelve un valor `String`
+    // correspondiente a la fecha de modificación almacenada en el atributo `FechaModifica` del objeto
+    // `TransferenciaDTO`.
     public String getFechaModifica() {
         return FechaModifica;
     }
     
+    // El método `public void setFechaModifica(String fechaModifica)` en la clase `TransferenciaDTO` es
+    // un método setter utilizado para establecer el valor del atributo `FechaModifica`, que representa
+    // la fecha de modificación de una transacción financiera. Este método toma un parámetro `String
+    // fechaModifica` y lo asigna al atributo `FechaModifica` del objeto `TransferenciaDTO`,
+    // actualizando efectivamente la fecha de modificación de la transacción.
     public void setFechaModifica(String fechaModifica) {
         FechaModifica = fechaModifica;
     }
- 
+    // El método `public String toString()` en la clase `TransferenciaDTO` anula el método `toString()`
+    // predeterminado proporcionado por la clase `Object` en Java.
     @Override
     public String toString() {
         return "\n" + getClass().getName() 
